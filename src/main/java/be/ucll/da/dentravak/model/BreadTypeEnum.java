fb736e31e42f01fa48cc0abd5f5ea5fb30df15ec
Rune Hanssens <rune.hanssens@hotmail.be>
2018-12-03 19:44:27 +0100

    order now adds order to db

--- src/main/java/be/ucll/da/dentravak/model/BreadTypeEnum.java
+++ src/main/java/be/ucll/da/dentravak/model/BreadTypeEnum.java
@@ -1,9 +1,14 @@
 package be.ucll.da.dentravak.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum SandwichType {
 
+    @JsonProperty("Turkish bread")
     TURKISH_BREAD("Turkish bread"),
+    @JsonProperty("Wrap")
     WRAP("Wrap"),
+    @JsonProperty("Boterhammekes")
     BOTERHAMMEKES("Boterhammekes");
 
     private String name;
