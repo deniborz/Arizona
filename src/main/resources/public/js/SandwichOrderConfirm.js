8fc72fdadca2aa0c6b4318242731fa6d6c2bd078
deniborz <deniborz@hotmail.com>
2019-01-05 15:18:24 +0100

    experimenten

--- src/main/resources/public/js/SandwichOrderConfirm.js
+++ src/main/resources/public/js/SandwichOrderConfirm.js
@@ -3,7 +3,29 @@ import MyCustomElement from './MyCustomElement.js'
 class SandwichOrderConfirm extends MyCustomElement{
   connectedCallback(){
     super.connectedCallback();
-    this.setupEventListeners();
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML = `saveRating(rating) {
+      let recommendedSandwich = {
+          emailAddress : "ronald.dehuysser@ucll.be",
+          ratedItem : this.order.sandwichId,
+          rating : rating
+      };
+
+      console.log(recommendedSandwich);
+
+      fetch('/recommendation/recommend/', {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json; charset=utf-8",
+          },
+          body: JSON.stringify(recommendedSandwich),
+      })
+          .then(resp => resp.json())
+          .then(response => alert('Bedankt!'));
+  }`
+    this.shadowRoot.appendChild(script);
+    //this.setupEventListeners();
   }
 
   orderTemplate(order){
@@ -15,11 +37,11 @@ class SandwichOrderConfirm extends MyCustomElement{
             </div>
             <div>
                 <p>Rating geven:</p>
-                <button class="btn btn-warning rating">1</button>
-                <button class="btn btn-warning rating">2</button>
-                <button class="btn btn-warning rating">3</button>
-                <button class="btn btn-warning rating">4</button>
-                <button class="btn btn-warning rating">5</button>
+                <a onclick="saveRating(1)" class="btn btn-warning rating">1</a>
+                <a onclick="saveRating(2)" class="btn btn-warning rating">2</a>
+                <a onclick="saveRating(3)" class="btn btn-warning rating">3</a>
+                <a onclick="saveRating(4)" class="btn btn-warning rating">4</a>
+                <a onclick="saveRating(5)" class="btn btn-warning rating">5</a>
                 </br></br>
                 <a href="index.html">Terug naar overzicht</a>
             </div>
